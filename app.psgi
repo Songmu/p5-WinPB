bc366764553c7f8dc87e0146ccf476f537e548ce
Masayuki Matsuki <y.songmu@gmail.com>
2012-05-22 00:54:36 +0900

    minimal framework

---
+++ app.psgi
@@ -0,0 +1,9 @@
+use strict;
+use warnings;
+use utf8;
+use lib './lib';
+
+use WinPB;
+
+WinPB->new->to_app;
+
--- lib/WinPB.pm
+++ lib/WinPB.pm
@@ -1,9 +1,64 @@
 package WinPB;
 use strict;
 use warnings;
+use utf8;
 our $VERSION = '0.01';
 
-1;
+use Any::Moose;
+use Router::Simple;
+use Plack::Request;
+use namespace::autoclean;
+
+has router => (
+    is => 'ro',
+    isa => 'Router::Simple',
+    default => sub { Router::Simple->new },
+    handles => [qw/connect match/],
+);
+
+sub register {
+    my $self = shift;
+    $self->connect( '/' => {
+        action  => sub {
+            my $req = shift;
+            'POST';
+        },
+    }, {method  => 'POST'});
+
+    $self->connect( '/' => {
+        action  => sub {
+            my $req = shift;
+            'GET'
+        },
+    }, {method  => 'GET'});
+
+}
+
+sub to_app {
+    my $self = shift;
+    return sub {
+        $self->process(@_);
+    };
+}
+
+sub process {
+    my ($self, $env) = @_;
+    $self->register;
+    my $matched = $self->match($env);
+
+    return [404, [], ['NOT FOUND']] unless $matched;
+
+    my $req = Plack::Request->new($env);
+    my $res = $req->new_response(200);
+    $res->content_type('text/plain: charset=utf8');
+    $res->body(
+        $matched->{action}->($req)
+    );
+    $res->finalize;
+}
+
+
+__PACKAGE__->meta->make_immutable;
 __END__
 
 =head1 NAME
